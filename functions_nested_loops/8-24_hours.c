726c260d49635b1fb96bdd960a7819b8dbd89892
root <gucci60610@gmail.com>
2024-10-22 14:26:49 +0000

    kk

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,6 +7,7 @@
 void jack_bauer(void)
 {
 	int i, j;
+
 	i = 0;
 
 	while (i < 24)
@@ -14,15 +15,14 @@ void jack_bauer(void)
 		j = 0;
 		while (j < 60)
 		{
-			_putchar((i / 10) + ('0'))
-			_putchar((i % 10) + ('0'))
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((j / 10) + ('0'))
-			_putchar((j % 10) + ('0'))
-			_putchar('\n')
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 			j++;
 		}
 		i++;
 	}
 }
-
