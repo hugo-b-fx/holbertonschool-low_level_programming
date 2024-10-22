81eb6a00bf80bedb8b96d5afdbe61b7ce200b3e9
root <gucci60610@gmail.com>
2024-10-22 12:54:25 +0000

    eee

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
 void print_alphabet_x10(void)
 {
 	char dix;
@@ -7,7 +12,7 @@ void print_alphabet_x10(void)
 
 	for (x = 1; x < 10; x++)
 	{
-		for (dix ='a'; dix <= 'z'; dix++)
+		for (dix = 'a'; dix <= 'z'; dix++)
 			_putchar(dix);
 	}
 	_putchar('\n');
