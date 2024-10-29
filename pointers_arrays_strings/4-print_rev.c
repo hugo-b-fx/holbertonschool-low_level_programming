fa8e13e2c4d61d9b1369999b5d8850200078c9fb
root <gucci60610@gmail.com>
2024-10-29 09:50:32 +0000

    d

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -21,6 +21,22 @@ void print_rev(char *s)
 	{
 		_putchar(s[j]);
 	}
+    int first = 0;
+    int last = strlen(str) - 1;
+    char temp;
 
+    // Swap characters till first and last meet
+    while (first < last) {
+      
+        // Swap characters
+        temp = str[first];
+        str[first] = str[last];
+        str[last] = temp;
+
+        // Move pointers towards each other
+        first++;
+        last--;
+    }
+}
 	_putchar('\n');
 }
