7f860c33e1bf967e3eabb5ab584badcd4d56c325
root <gucci60610@gmail.com>
2024-10-29 09:53:55 +0000

    dd

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,12 +4,8 @@
  * print_rev - prints a string in reverse, followed by a new line
  * @s: string to be printed
  */
-#include "main.h"
 
-/**
- * print_rev - prints a string in reverse, followed by a new line
- * @s: string to be printed
- */
+
 void print_rev(char *s)
 {
 	int i, j, len;
