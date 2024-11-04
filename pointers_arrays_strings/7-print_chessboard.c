8d2ffe43ac74cf057d898a1a26abeae8fcf338a1
root <gucci60610@gmail.com>
2024-11-04 14:20:19 +0000

    ddd

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -5,18 +5,17 @@
 * @a: 2D array
 */
 
-
 void print_chessboard(char (*a)[8])
 {
-	int row;
-	int column;
 
-	for (row = 0; row < 8; row++)
-	{
-		for (column = 0; column < 0; column++)
-		{
-			_putchar(a[row][column]);
-			_putchar('\n');
-		}
-	}
+int row;
+int column;
+
+for (row = 0; row < 8; row++)
+{
+for (column = 0; column < 8; column++)
+_putchar(a[row][column]);
+_putchar('\n');
 }
+}
+
