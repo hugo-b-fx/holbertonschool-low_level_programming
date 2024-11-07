4dfe0fd8bcd62e0653e6c907bb0f898cbb952f63
root <gucci60610@gmail.com>
2024-11-07 11:39:14 +0000

    diddy

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the numbers
+ * @argc: argument count
+ * @argv: aragument vector
+ *
+ * Return: alwys 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
