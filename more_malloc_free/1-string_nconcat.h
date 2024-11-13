03c7cea98501208e8c91cf8e014520b9370d614c
root <gucci60610@gmail.com>
2024-11-13 14:23:28 +0000

    dd

--- more_malloc_free/1-string_nconcat.h
+++ more_malloc_free/1-string_nconcat.h
@@ -1,48 +1,52 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * string_nconcat - function to concatnate strings with n bytes
- * @s1: destination for concatnation
- * @s2: source of string
- * @n: int type for size of byte
- * Return: pointer to new memory allocated
+ * string_nconcat - point
+ * @s1: first char
+ * @s2: second char
+ * @n: stop of s2
+ * Return: Always 1 (True) or 0 (False)
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int count, count1;
-	int sign = n;
-	char *ptr;
-	int len1, len2;
+	unsigned int len_s1 = 0;
+	unsigned int len_displayed_s2 = 0;
+	char *mal;
+	unsigned int totalSize = 0;
+	unsigned int index = 0;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
+	while (s1[len_s1] != '\0')
+		len_s1++;
 
-	if (sign >= len2)
+	while (s2[len_displayed_s2] != '\0')
+		len_displayed_s2++;
+
+	if (n < len_displayed_s2)
+		len_displayed_s2 = n;
+	totalSize = len_s1 + len_displayed_s2;
+
+	mal = malloc(totalSize + 1);
+
+	if (mal == 0)
 	{
-		sign = len2;
-		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-	}
-	else
-		ptr = malloc(sizeof(char) * (len1 + n + 1));
-	if (ptr == NULL)
+		free(mal);
 		return (NULL);
-	for (count = 0; count < len1; count++)
-	{
-		ptr[count] = s1[count];
 	}
-	for (count1 = 0; count1 < sign; count1++)
+	for (; index < totalSize; index++)
 	{
-		ptr[count++] = s2[count1];
+		if (index < len_s1)
+			mal[index] = s1[index];
+		else if (index - len_s1 < n)
+			mal[index] = s2[index - len_s1];
 	}
-	ptr[count++] = '\0';
-	return (ptr);
+	mal[index] = '\0';
+
+	return (mal);
 }
