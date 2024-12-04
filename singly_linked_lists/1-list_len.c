3fc8398b9e593f4f7c6f2326e8f5e38c935f9706
root <gucci60610@gmail.com>
2024-12-04 01:26:03 +0000

    betty style

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 
 /**
- *  * list_len - return number of element
- *   * @h : the content of element
- *    *
- *     * Return: element
- *      */
+ * list_len - return number of element
+ * @h : the content of element
+ * Return: element
+ */
 
 size_t list_len(const list_t *h)
 {
