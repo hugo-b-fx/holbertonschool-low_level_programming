dd332e72ca3099d1dd322104c7c7043200a0f951
root <gucci60610@gmail.com>
2024-12-04 01:12:17 +0000

    task n1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *  * list_len - return number of element
+ *   * @h : the content of element
+ *    *
+ *     * Return: element
+ *      */
+
+size_t list_len(const list_t *h)
+{
+	int x = 0;
+
+	while (h)
+	{
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
