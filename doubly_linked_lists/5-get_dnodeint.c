bce64b7907bf485304284282cdd2f7cd82930ee5
root <gucci60610@gmail.com>
2024-12-06 09:59:35 +0000

    typo in desc  of task 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- *  * get_dnodeint_at_index - return one node chose
- *   * @head : the content of element
- *    * @index : the node we choose
- *     * Return: head
- *      */
+ * get_dnodeint_at_index - return one node chose
+ * @head : the content of element
+ * @index : the node we choose
+ * Return: head
+ */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
