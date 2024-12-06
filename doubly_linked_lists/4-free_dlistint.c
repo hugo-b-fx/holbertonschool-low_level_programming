45cdd7823105dbc1319b96db0c2f028a9caeb4ef
root <gucci60610@gmail.com>
2024-12-06 09:43:26 +0000

    task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_dlistint - free the list
+ * @head : the content of element
+ *
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *jsp;
+
+	while (head)
+	{
+		palace = head->next;
+		free(head);
+		head = jsp;
+	}
+}
